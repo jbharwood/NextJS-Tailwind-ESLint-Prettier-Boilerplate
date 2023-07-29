fe2ad96f0cfc19dd0fc0f4eeb05a337b5d5efb91
Joseph Harwood <josephblakeharwood@gmail.com>
2023-07-29 15:11:10 -0400

    init

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <main>
+            <Head>
+                <title>NextJS App</title>
+            </Head>
+            <span className="text-red-500">Hello World</span>
+        </main>
+    )
+}
